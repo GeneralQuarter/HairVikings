1be7a761891a482136e285e289960655dbb0f92f
x1QG1x <ViveWarHammer128>
2016-11-14 15:54:34 +0000

    Corrected merge

--- src/graph/CellType.java
+++ src/graph/CellType.java
@@ -3,6 +3,7 @@ package graph;
 /**
  * Created by t00191774 on 14/11/2016.
  * Made by Roland -> http://stackoverflow.com/questions/30679025/graph-visualisation-like-yfiles-in-javafx
+ *
  */
 public enum CellType {
     RECTANGLE,
