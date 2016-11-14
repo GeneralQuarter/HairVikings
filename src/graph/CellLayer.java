634694f0b897ae1cf66dcf3dc0be4a7948e0ac22
t00191729 <hugo.constant42@gmail.com>
2016-11-14 15:28:10 +0000

    Added Copyrights

--- src/graph/CellLayer.java
+++ src/graph/CellLayer.java
@@ -2,6 +2,7 @@ package graph;
 
 /**
  * Created by t00191729 on 14/11/2016.
+ * Made by Roland -> http://stackoverflow.com/questions/30679025/graph-visualisation-like-yfiles-in-javafx
  */
 
 import javafx.scene.layout.Pane;
--- src/layout/Layout.java
+++ src/layout/Layout.java
@@ -2,6 +2,7 @@ package layout;
 
 /**
  * Created by t00191729 on 14/11/2016.
+ * Made by Roland -> http://stackoverflow.com/questions/30679025/graph-visualisation-like-yfiles-in-javafx
  */
 
 public abstract class Layout {
